503a0f54f919a749ede3b0a3d1a92825847f1caf
Luis Montero <luismontero@hotmail.fr>
2020-03-08 23:03:41 +0100

    update

--- graph_doc2vec.py
+++ graph_doc2vec.py
@@ -19,14 +19,18 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 import warnings
 from joblib import dump, load
+from utils import get_raw_data
 
 def warn(*args, **kwargs):
     pass
 
+import os
+print(os.getcwd())
+
 warnings.warn = warn
 
-train_hosts, test_hosts, y_train, G = load_data()
-
+# train_hosts, test_hosts, y_train, G = load_data()
+train_data, test_data, y_train, G, train_hosts, test_hosts = get_raw_data()
 # word_to_idx = make_vocab(documents)
 
 # documents_ints = documents_to_idx(documents, word_to_idx)
@@ -37,12 +41,13 @@ train_hosts, test_hosts, y_train, G = load_data()
 # print(np.mean([len(doc) for doc in documents.values()]))
 
 
+
 print(G.number_of_nodes())
 print(G.number_of_edges())
 
 
 # Node embeddings
-make_embeddings = Falsepuyt
+make_embeddings = False
 n_features = 256
 n_walks = 150
 walk_length = 100
@@ -74,14 +79,18 @@ if _visualize_embeddings:
 # else:
 #     doc_embeddings = Doc2Vec.load('graph_models/node_embedding/doc_embeddings.model')
 
+
+doc_embeddings_train = np.load("doc2vec_xtrain.pkl.npy")
 X_train = embedder.transform(train_hosts)
+print(X_train.shape, doc_embeddings_train.shape)
+X_train = np.concatenate((X_train, doc_embeddings_train), axis=1)  
+print(X_train.shape)
 
-
-# X_train = np.concatenate((X_train, doc_embeddings_train.toarray()), axis=1)  
-
+doc_embeddings_test = np.load("doc2vec_xtest.pkl.npy")
 X_test = embedder.transform(test_hosts)
-# X_test = np.concatenate((X_test, doc_embeddings_test.toarray()), axis=1)
-
+print(X_test.shape, doc_embeddings_test.shape)
+X_test = np.concatenate((X_test, doc_embeddings_test), axis=1)
+print(X_test.shape)
 
 print("Train matrix dimensionality: ", X_train.shape)
 print("Test matrix dimensionality: ", X_test.shape)
@@ -137,13 +146,15 @@ for solver in solvers:
         for max_iter in max_iters:
             for tol in tols:
                 for C in Cs:
-                    classifiers.append(LogisticRegression(
+                    classifiers.append(CalibratedClassifierCV(
+                        LogisticRegression(
                         penalty=penalty,
                         tol=tol, 
                         C=C,
                         solver=solver,
                         max_iter=max_iter,
-                        multi_class=multi_classe))
+                        multi_class=multi_classe), cv=7))
+
 
 print(len(classifiers))
 
